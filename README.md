c457aa22e074f5c69fde7c4f54b9a81ec8e01270
banagg <anurag.bansal.585@gmail.com>
2019-03-31 12:36:45 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# InstaAPIv1
+
+
+InstaAPIv1 is an API endpoint which extracts info about images from Instagram when given a Brand Name & a Product Name. Implemented using the Instagram scraping API whose link can be found [here](https://github.com/rarcega/instagram-scraper) (which is necessary to install to successfully run it). The scraper scrapes 100 items for both product name and the brand name and its maximum value can be changed manually from the code by changing the value in the -m paramater (though increasing the value will increase the time taken by the API to run).
+
+Technologies Used: Python
+Libraries Used: Flask, sqlite3, subprocess, pandas
+
+Steps to operate:
+
+1 - Run the mainf.py file
+
+2 - Run the hello.html file which can be found in the templates section.
+
+3 - Enter the Brand Name and the Product Name on the hello.html page.
+
+4 - Wait for the scraping to end and view the extracted data.
---
+++ mainf.py
@@ -0,0 +1,83 @@
+import subprocess
+from flask import Flask, redirect, url_for, request, jsonify
+import sqlite3
+import pandas as pd
+import os
+
+app = Flask(__name__)
+
+def dict_factory(cursor, row):
+	d = {}
+	for idx, col in enumerate(cursor.description):
+		d[col[0]] = row[idx]
+	return d
+
+@app.route('/api/v1/info', methods=['GET'])
+def api_all():
+	conn = sqlite3.connect('database.db')
+	cur = conn.cursor()
+	conn.row_factory = dict_factory
+	info = cur.execute('SELECT p.id, p.url, p.owner_id FROM pro_name as p, bd_name WHERE p.id = bd_name.id').fetchall()
+	
+	f_info = []
+	tag_info = []
+	c_info = {}
+	for i in range(0,len(info)):
+		tag_info = cur.execute('SELECT distinct tag_tb.tag FROM tag_tb WHERE tag_tb.id = %s' % info[i][0]).fetchall()
+		ftag_info = [y for x in tag_info for y in x]
+		c_info = { 'id': info[i][0], 'url': info[i][1], 'owner_id': info[i][2], 'tags': ftag_info }
+		f_info.append(c_info)
+	
+	return jsonify(f_info)
+
+@app.route('/ezenciel/')
+def success():
+   return 'Currently not an active functionality'
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      pname = request.form['pn']
+      brand = request.form['bd']
+      subprocess.call('rm -rf %s' % pname, shell = True)
+      subprocess.call('rm -rf %s' % brand, shell = True)
+      subprocess.call('instagram-scraper %s --tag --media-metadata --media-types none -m 100' % pname, shell = True)
+      subprocess.call('instagram-scraper %s --tag --media-metadata --media-types none -m 100' % brand, shell = True)
+
+      conn = sqlite3.connect('database.db')
+      cur = conn.cursor()
+      conn.execute('DELETE FROM pro_name')
+      conn.commit()
+      conn.execute('DELETE FROM bd_name')
+      conn.commit()
+      conn.execute('DELETE FROM tag_tb')
+      conn.commit()
+
+      with open(os.getcwd() + '/%s/%s.json' % (pname, pname)) as f:
+      	df = pd.read_json(f)
+      with open(os.getcwd() + '/%s/%s.json' % (brand, brand)) as g:
+      	dg = pd.read_json(g)
+
+      for i in range(0,len(df['GraphImages'])):
+      	conn.execute("INSERT OR IGNORE INTO pro_name (id, url, owner_id) VALUES (?, ?, ?)", (df['GraphImages'][i]['id'], df['GraphImages'][i]['display_url'], df['GraphImages'][i]['owner']['id']))
+      conn.commit()
+      for i in range(0,len(df['GraphImages'])):
+      	try:
+      		for j in range(0,len(df['GraphImages'][i]['tags'])):
+      			conn.execute("INSERT INTO tag_tb (id, tag) VALUES (?, ?)", (df['GraphImages'][i]['id'], df['GraphImages'][i]['tags'][j]))
+      	except KeyError:
+      		pass
+      conn.commit()
+      for i in range(0,len(dg['GraphImages'])):
+      	conn.execute("INSERT INTO bd_name (id) VALUES (?)", (dg['GraphImages'][i]['id'],))
+      conn.commit()
+      conn.close()
+
+      return redirect(url_for('api_all'))
+   else:
+      pname = request.args.get('pn')
+      brand = request.args.get('bd')
+      return redirect(url_for('success'))
+
+if __name__ == '__main__':
+   app.run(debug = True)
